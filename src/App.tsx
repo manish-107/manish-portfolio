f9b88520c2fa12c0ae5676e5bb8d6725747f81c4
manish-107 <creativecrew2676@gmail.com>
2024-06-08 12:30:46 +0530

    tamplate setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import  Nav  from "./components/Nav.tsx";
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+
+const Nav = () => {
+  return (
+    <div className="text-black bg-slate-600">
+      <div>hello</div>
+      <div>log out</div>
+    </div>
+  )
+}
+
+export default Nav
