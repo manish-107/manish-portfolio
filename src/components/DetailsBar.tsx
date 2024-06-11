c2f7cf6cc3ffffea7e0103bd7a482ac887175b05
manish-107 <creativecrew2676@gmail.com>
2024-06-11 23:00:27 +0530

    social media linked

--- src/components/DetailsBar.tsx
+++ src/components/DetailsBar.tsx
@@ -2,14 +2,62 @@
 const DetailsBar = () => {
   return (
     <div className="ml-0 md:ml-56">
-        
-<a className="block max-w-3xl shadow bg-transparent-lg">
-  <p className="pl-4 pr-4 font-normal text-gray-400">A dedicated software developer specializing in JavaScript, ReactJS, and NodeJs. With a strong background in building dynamic, high-performance web applications. Passionate about coding and problem-solving. I am committed to delivering robust and innovative software solutions.</p>
-</a>
-    
+      <a className="block max-w-3xl shadow bg-transparent-lg">
+        <p className="pl-4 pr-4 font-normal text-gray-400">
+          A dedicated software developer specializing in JavaScript, ReactJS, and NodeJs. With a strong background in building dynamic, high-performance web applications. Passionate about coding and problem-solving. I am committed to delivering robust and innovative software solutions.
+        </p>
+      </a>
 
+      <section className="flex flex-col md:flex-row">
+        <div className="flex m-6">
+          <div className="flex flex-wrap justify-center font-semibold ">
+           
+
+            <button  className="flex flex-row self-center px-3 py-2 mx-2 my-2 text-xs text-center text-white bg-blue-600 rounded-lg">
+              <svg
+                className="mr-1"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="20"
+                height="20"
+                viewBox="0 0 48 48"
+                fill="none"
+              >
+                <path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path>
+                <path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"></path>
+              </svg>
+              <a href='https://www.linkedin.com/in/manish-kulal-96518a2b5/'>LinkedIn</a>
+            </button>
+ <button className="flex flex-row self-center px-3 py-2 mx-2 mr-2 text-xs text-center text-white bg-black rounded-lg">
+              <svg
+                className="mr-1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 2c-5.523 0 -10 4.477 -10 10c0 4.418 2.865 8.167 6.839 9.489c.5 .092 .682 -.217 .682 -.483c0 -.237 -.009 -.868 -.014 -1.703c-2.782 .604 -3.369 -1.34 -3.369 -1.34c-.454 -1.155 -1.11 -1.463 -1.11 -1.463c-.908 -.62 .069 -.607 .069 -.607c1.003 .07 1.531 1.031 1.531 1.031c.892 1.529 2.341 1.088 2.91 .832c.092 -.647 .349 -1.088 .635 -1.338c-2.22 -.252 -4.555 -1.11 -4.555 -4.942c0 -1.091 .39 -1.982 1.029 -2.68c-.103 -.253 -.446 -1.272 .098 -2.65c0 0 .84 -.269 2.75 1.026a9.562 9.562 0 0 1 2.5 -.336c.849 .004 1.705 .115 2.5 .336c1.91 -1.295 2.75 -1.026 2.75 -1.026c.545 1.378 .202 2.397 .1 2.65c.64 .698 1.028 1.589 1.028 2.68c0 3.842 -2.337 4.687 -4.565 4.932c.359 .309 .678 .919 .678 1.853c0 1.337 -.012 2.417 -.012 2.746c0 .269 .18 .579 .688 .481c3.974 -1.322 6.839 -5.071 6.839 -9.489c0 -5.523 -4.477 -10 -10 -10z"></path>
+              </svg>
+              <a href='https://github.com/manish-107'>Github</a>
+            </button>
+            <button className="flex flex-row self-center px-3 py-2 mx-2 my-2 text-xs text-center text-black bg-white rounded-lg">
+             <svg xmlns="http://www.w3.org/2000/svg" className='p-0 mr-1' x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+<path d="M 11 4 C 7.1456661 4 4 7.1456661 4 11 L 4 39 C 4 42.854334 7.1456661 46 11 46 L 39 46 C 42.854334 46 46 42.854334 46 39 L 46 11 C 46 7.1456661 42.854334 4 39 4 L 11 4 z M 11 6 L 39 6 C 41.773666 6 44 8.2263339 44 11 L 44 39 C 44 41.773666 41.773666 44 39 44 L 11 44 C 8.2263339 44 6 41.773666 6 39 L 6 11 C 6 8.2263339 8.2263339 6 11 6 z M 13.085938 13 L 22.308594 26.103516 L 13 37 L 15.5 37 L 23.4375 27.707031 L 29.976562 37 L 37.914062 37 L 27.789062 22.613281 L 36 13 L 33.5 13 L 26.660156 21.009766 L 21.023438 13 L 13.085938 13 z M 16.914062 15 L 19.978516 15 L 34.085938 35 L 31.021484 35 L 16.914062 15 z"></path>
+</svg>
+              <a href='https://twitter.com/Mani_xsh'>Twitter</a>
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default DetailsBar
+export default DetailsBar;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,11 @@
 const Nav = () => {
   return (
-   <header className="fixed z-50 flex items-center justify-center w-11/12 p-5 m-5 mx-auto mb-56 ml-5 text-gray-100 rounded-sm bg-opacity-5 md:ml-16 bg-slate-400 backdrop-blur-md">
+   <header className="fixed z-50 flex items-center justify-center w-11/12 p-5 m-5 mx-auto mb-56 ml-5 text-gray-100 rounded-2xl bg-opacity-5 md:ml-16 bg-slate-400 backdrop-blur-md">
      <div className="flex space-x-5 font-semibold md:font-extrabold md:space-x-10">
        <div>About</div>
+       <div>Skills</div>
        <div>Projects</div>
        <div>Contact</div>
-       <div>Mode</div>
      </div>
    </header>
   );
